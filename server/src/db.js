fe144c8392f80e7fb72c84339de6a4e1ad23d06b
GitHub-Laziji <c_smtp@163.com>
2020-02-10 22:36:28 +0800

    init

---
+++ server/src/db.js
@@ -0,0 +1,117 @@
+const Promise = require("bluebird");
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database("../data.db");
+
+function baseModel(tableName, columns) {
+  let columnSet = new Set(columns);
+  return {
+    insert(row) {
+      for (let k in row) {
+        if (!columnSet.has(k)) {
+          return Promise.reject();
+        }
+      }
+      let ps = "";;
+      for (let i = 0; i < columns.length; i++) {
+        ps += ",?";
+      }
+      ps = ps.substring(1);
+      let params = [];
+      for (let k of columns) {
+        params.push(row[k]);
+      }
+      return new Promise((resolve, reject) => {
+        db.run.apply(db, [
+          `insert into ${tableName}(${columns.join(",")}) values(${ps})`,
+          ...params,
+          (err, res) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(res);
+          }
+        ]);
+      })
+    },
+    updateById(row) {
+      let sets = "";
+      let params = [];
+      for (let k in row) {
+        if (!columnSet.has(k)) {
+          return Promise.reject();
+        }
+        sets += ` , ${k}=`;
+        if (row[k] !== null) {
+          sets += "?";
+          params.push(row[k]);
+        } else {
+          sets += "null";
+        }
+      }
+      sets = sets.substring(2);
+      return new Promise((resolve, reject) => {
+        db.all.apply(db, [
+          `update ${tableName} set ${sets} where id=?`,
+          ...params,
+          row.id,
+          (err, res) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(res);
+          }
+        ]);
+      });
+    },
+    selectById(id) {
+      return new Promise((resolve, reject) => {
+        db.all.apply(db, [
+          `select * from ${tableName} where id=?`,
+          id,
+          (err, res) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(res[0]);
+          }
+        ]);
+      });
+    },
+    selectAll(query) {
+      let wheres = "";
+      let params = [];
+      for (let k in query) {
+        if (!columnSet.has(k)) {
+          return Promise.reject();
+        }
+        wheres += ` and ${k}`;
+        if (query[k] === null) {
+          wheres += " is null";
+        } else {
+          wheres += "=?";
+          params.push(query[k]);
+        }
+
+      }
+      wheres = wheres.substring(4);
+      return new Promise((resolve, reject) => {
+        db.all.apply(db, [
+          `select * from ${tableName} where ${wheres}`,
+          ...params,
+          (err, res) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(res);
+          }
+        ]);
+      });
+    }
+  }
+}
+
+const User = baseModel("user", ["id", "name"]);
+
+module.exports = {
+  User
+}
